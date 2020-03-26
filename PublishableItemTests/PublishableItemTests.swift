c69eee5b053fdfe278037bc4bfd837f309a89c2f
David Thorn <thorn@adesso-mobile.de>
2020-03-26 08:28:06 +0100

    master: Added replay subject basic unit tests

--- PublishableItemTests/PublishableItemTests.swift
+++ PublishableItemTests/PublishableItemTests.swift
@@ -95,4 +95,46 @@ class PublishableItemTests: XCTestCase {
         XCTAssertEqual(mockPerson.name.value, "Thorn")
     }
 
+    enum MockError: Error {
+        case error
+    }
+
+    /// Creating test to be sure that I understand how the Replay Subject works so that it can be converted to Replayable Items.
+    func testReplaySubject() {
+        let scheduler = TestScheduler(initialClock: 0)
+        let observer = scheduler.createObserver(String.self)
+        let observer1 = scheduler.createObserver(String.self)
+
+        let replaySubject = ReplaySubject<String>.create(bufferSize: 3)
+        replaySubject.subscribe(observer).disposed(by: disposeBag)
+
+        replaySubject.on(.next("One"))
+
+        scheduler.scheduleAt(100) {
+            replaySubject.on(.next("Two"))
+        }
+
+        scheduler.scheduleAt(200) {
+            replaySubject.on(.error(MockError.error))
+        }
+
+        scheduler.scheduleAt(300) {
+            replaySubject.on(.next("Three"))
+        }
+
+        replaySubject.subscribe(observer1).disposed(by: disposeBag)
+
+        scheduler.start()
+
+        let recorded: [Recorded<Event<String>>] = [
+            .next(0, "One"),
+            .next(100, "Two"),
+            .error(200, MockError.error)
+        ]
+
+        XCTAssertEqual(observer1.events, recorded)
+        XCTAssertEqual(observer.events, recorded)
+        scheduler.stop()
+    }
+
 }
